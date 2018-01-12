d76d00a353eb146ebf5cd050f752e0158dbfc470
Chris Laprun <metacosm@gmail.com>
2018-01-12 17:40:21 +0100

    Add initial Java type association to ValueTypes.

--- istio-model/src/main/java/me/snowdrop/istio/api/model/v1/mixer/config/descriptor/ValueType.java
+++ istio-model/src/main/java/me/snowdrop/istio/api/model/v1/mixer/config/descriptor/ValueType.java
@@ -6,6 +6,13 @@
  */
 package me.snowdrop.istio.api.model.v1.mixer.config.descriptor;
 
+import java.net.InetAddress;
+import java.net.URI;
+import java.util.Date;
+import java.util.Map;
+
+import me.snowdrop.istio.api.model.Duration;
+
 /**
  * ValueType describes the types that values in the Istio system can take. These
  * are used to describe the type of Attributes at run time, describe the type of
@@ -19,49 +26,55 @@ public enum ValueType {
     /**
      * Invalid, default value.
      */
-    VALUE_TYPE_UNSPECIFIED,
+    VALUE_TYPE_UNSPECIFIED(null),
     /**
      * An undiscriminated variable-length string.
      */
-    STRING,
+    STRING(String.class),
     /**
      * An undiscriminated 64-bit signed integer.
      */
-    INT64,
+    INT64(Integer.class),
     /**
      * An undiscriminated 64-bit floating-point value.
      */
-    DOUBLE,
+    DOUBLE(Double.class),
     /**
      * An undiscriminated boolean value.
      */
-    BOOL,
+    BOOL(Boolean.class),
     /**
      * A point in time.
      */
-    TIMESTAMP,
+    TIMESTAMP(Date.class),
     /**
      * An IP address.
      */
-    IP_ADDRESS,
+    IP_ADDRESS(InetAddress.class),
     /**
      * An email address.
      */
-    EMAIL_ADDRESS,
+    EMAIL_ADDRESS(String.class), // todo: create Email class
     /**
      * A URI.
      */
-    URI,
+    URI(URI.class),
     /**
      * A DNS name.
      */
-    DNS_NAME,
+    DNS_NAME(String.class), // todo: not sure what this represents exactly or how to validate
     /**
      * A span between two points in time.
      */
-    DURATION,
+    DURATION(Duration.class),
     /**
      * A map string - string, typically used by headers.
      */
-    STRING_MAP
+    STRING_MAP(Map.class);
+
+    ValueType(Class associatedType) {
+        this.associatedType = associatedType;
+    }
+
+    private final Class associatedType;
 }
