fba2dd3963632a98b98f0462eb7b2698f2f5cfcc
Chris Laprun <metacosm@gmail.com>
2018-01-16 22:45:15 +0100

    Improve tests and add aggregate descriptor test.

--- istio-client/src/test/java/me/snowdrop/istio/client/IstioClientTest.java
+++ istio-client/src/test/java/me/snowdrop/istio/client/IstioClientTest.java
@@ -1,27 +1,23 @@
 package me.snowdrop.istio.client;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-import me.snowdrop.istio.api.internal.IstioSpecRegistry;
 import me.snowdrop.istio.api.model.IstioResource;
+import me.snowdrop.istio.api.model.IstioSpec;
+import me.snowdrop.istio.api.model.v1.routing.DestinationPolicy;
+import me.snowdrop.istio.api.model.v1.routing.EgressRule;
+import me.snowdrop.istio.api.model.v1.routing.RouteRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static me.snowdrop.istio.api.internal.IstioSpecRegistry.getCRDNameFor;
+import static me.snowdrop.istio.api.internal.IstioSpecRegistry.getKindFor;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyIterable;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -34,59 +30,56 @@ public class IstioClientTest {
 
     @Test
     public void shouldApplyRouteRuleIstioResource() {
-
-        // Given
-        final IstioClient istioExecutor = new IstioClient(adapter);
-        final InputStream routeRule = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("route-rule.yaml");
-
-        // When
-        when(adapter.createCustomResources(any(IstioResource.class))).thenReturn(Collections.singletonList(new IstioResource()));
-
-        // Then
-        final List<IstioResource> istioResource = istioExecutor.registerCustomResources(routeRule);
-        assertThat(istioResource).isNotEmpty();
-        assertThat(istioResource.size()).isEqualTo(1);
-        verify(adapter, times(1)).createCustomResources(any(IstioResource.class));
-
+        checkInput("route-rule.yaml", RouteRule.class);
     }
 
     @Test
     public void shouldApplyDestinationPolicyIstioResource() {
-
-        // Given
-        final IstioClient istioExecutor = new IstioClient(adapter);
-        final InputStream destinationPolicy = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("destination-policy.yaml");
-
-        // When
-        when(adapter.createCustomResources(any(IstioResource.class))).thenReturn(Collections.singletonList(new IstioResource()));
-
-        // Then
-        final List<IstioResource> istioResource = istioExecutor.registerCustomResources(destinationPolicy);
-        assertThat(istioResource).isNotEmpty();
-        assertThat(istioResource.size()).isEqualTo(1);
-        verify(adapter, times(1)).createCustomResources(any(IstioResource.class));
-
+        checkInput("destination-policy.yaml", DestinationPolicy.class);
     }
 
     @Test
     public void shouldApplyEgressRuleIstioResource() {
+        checkInput("egress-rule.yaml", EgressRule.class);
+    }
 
+    private void checkInput(String inputFileName, Class<? extends IstioSpec> expectedSpecClass) {
         // Given
-        final IstioClient istioExecutor = new IstioClient(adapter);
-        final InputStream egressRule = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("egress-rule.yaml");
+        final IstioClient client = new IstioClient(adapter);
+        final InputStream inputStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(inputFileName);
 
         // When
-        when(adapter.createCustomResources(any(IstioResource.class))).thenReturn(Collections.singletonList(new IstioResource()));
+        when(adapter.createCustomResources(any())).thenAnswer(invocation -> Collections.singletonList(invocation.getArgument(0)));
 
         // Then
-        final List<IstioResource> istioResource = istioExecutor.registerCustomResources(egressRule);
-        assertThat(istioResource).isNotEmpty();
-        assertThat(istioResource.size()).isEqualTo(1);
+        final List<IstioResource> result = client.registerCustomResources(inputStream);
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getKind()).isEqualTo(getKindFor(expectedSpecClass));
         verify(adapter, times(1)).createCustomResources(any(IstioResource.class));
+    }
 
+    @Test
+    public void shouldApplyAllResourcesInAggregateDescriptor() {
+
+        // Given
+        final IstioClient client = new IstioClient(adapter);
+        final InputStream aggregate = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("aggregate.yaml");
+
+
+        // When
+        when(adapter.createCustomResources(any())).thenAnswer(invocation -> Arrays.asList(invocation.getArguments()));
+
+        // Then
+        final List<IstioResource> result = client.registerCustomResources(aggregate);
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0).getKind()).isEqualTo(getKindFor(DestinationPolicy.class));
+        assertThat(result.get(1).getKind()).isEqualTo(getKindFor(EgressRule.class));
+        assertThat(result.get(2).getKind()).isEqualTo(getKindFor(RouteRule.class));
+        verify(adapter, times(1)).createCustomResources(any());
     }
 
 }
