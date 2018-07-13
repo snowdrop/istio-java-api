bea0379e686a914765227a4891d72d294dbeadee
Chris Laprun <metacosm@gmail.com>
2018-07-13 15:47:08 +0200

    Remove redundant @JsonDeserialize.

--- istio-model/src/main/java/me/snowdrop/istio/api/model/v1/networking/ExactStringMatch.java
+++ istio-model/src/main/java/me/snowdrop/istio/api/model/v1/networking/ExactStringMatch.java
@@ -8,7 +8,6 @@ package me.snowdrop.istio.api.model.v1.networking;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.fabric8.kubernetes.api.model.Doneable;
 import io.sundr.builder.annotations.Buildable;
 import io.sundr.builder.annotations.Inline;
@@ -19,7 +18,6 @@ import lombok.ToString;
  * @author <a href="claprun@redhat.com">Christophe Laprun</a>
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-@JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
 @ToString
 @EqualsAndHashCode
 @Buildable(editableEnabled = false, validationEnabled = true, generateBuilderPackage = true, builderPackage = "io.fabric8.kubernetes.api.builder", inline = @Inline(type = Doneable.class, prefix = "Doneable", value = "done"))
