3998d6b24f0fe364948c8e2387a25c9f75a290af
Chris Laprun <metacosm@gmail.com>
2018-10-11 14:21:00 +0200

    Fix PolicyTest set up.

--- istio-model/src/test/java/me/snowdrop/istio/api/authentication/v1alpha1/PolicyTest.java
+++ istio-model/src/test/java/me/snowdrop/istio/api/authentication/v1alpha1/PolicyTest.java
@@ -1,14 +1,13 @@
 package me.snowdrop.istio.api.authentication.v1alpha1;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.InputStream;
 import java.util.List;
-import me.snowdrop.istio.api.networking.v1alpha3.NamePort;
-import me.snowdrop.istio.api.networking.v1alpha3.PortSelector.Port;
+
 import me.snowdrop.istio.tests.BaseIstioTest;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PolicyTest extends BaseIstioTest {
 
@@ -26,12 +25,19 @@ public class PolicyTest extends BaseIstioTest {
     assertEquals(1, targets.size());
     final TargetSelector target = targets.get(0);
     final List<PortSelector> portSelectors = target.getPorts();
-    assertEquals(1, portSelectors.size());
-    final PortSelector portSelector = portSelectors.get(0);
-    final Port port = portSelector.getPort();
+    assertEquals(2, portSelectors.size());
+
+    PortSelector portSelector = portSelectors.get(0);
+    PortSelector.Port port = portSelector.getPort();
     assertTrue(NamePort.class.isAssignableFrom(port.getClass()));
     final NamePort namePort = (NamePort) port;
     assertEquals("https", namePort.getName());
+
+    portSelector = portSelectors.get(1);
+    port = portSelector.getPort();
+    assertTrue(NumberPort.class.isAssignableFrom(port.getClass()));
+    final NumberPort numberPort = (NumberPort) port;
+    assertEquals(443, (int) numberPort.getNumber());
   }
 
 }
