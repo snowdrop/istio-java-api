ab7ebfb828e15f82217a7e0beb7ae8b555f0d9cb
Chris Laprun <metacosm@gmail.com>
2020-05-18 18:25:06 +0200

    feat: re-add generation for v1alpha3 networking entities

---
+++ istio-model/src/main/java/me/snowdrop/istio/api/networking/v1alpha3/CaptureMode.java
@@ -0,0 +1,56 @@
+/*
+ *
+ *  * Copyright (C) 2019 Red Hat, Inc.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *         http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ *
+ */
+package me.snowdrop.istio.api.networking.v1alpha3;
+
+/**
+ * CaptureMode describes how traffic to a listener is expected to be
+ * captured. Applicable only when the listener is bound to an IP.
+ *
+ * @author <a href="claprun@redhat.com">Christophe Laprun</a>
+ */
+public enum CaptureMode {
+    /**
+     * The default capture mode defined by the environment
+     */
+    CaptureMode_DEFAULT(0),
+
+    /**
+     * Capture traffic using IPtables redirection
+     */
+    CaptureMode_IPTABLES(1),
+
+    /**
+     * No traffic capture. When used in egress listener, the application is
+     * expected to explicitly communicate with the listener port/unix
+     * domain socket. When used in ingress listener, care needs to be taken
+     * to ensure that the listener port is not in use by other processes on
+     * the host.
+     */
+    CaptureMode_NONE(2);
+
+    private final int intValue;
+
+    CaptureMode(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int value() {
+        return intValue;
+    }
+}
---
+++ istio-model/src/main/java/me/snowdrop/istio/api/networking/v1alpha3/TLSOptionsMode.java
@@ -0,0 +1,56 @@
+package me.snowdrop.istio.api.networking.v1alpha3;
+
+public enum TLSOptionsMode {
+
+    /**
+     * The SNI string presented by the client will be used as the match
+     * criterion in a VirtualService TLS route to determine the
+     * destination service from the service registry.
+     */
+    PASSTHROUGH(0),
+
+    /**
+     * Secure connections with standard TLS semantics.
+     */
+    SIMPLE(1),
+
+    /**
+     * Secure connections to the downstream using mutual TLS by presenting
+     * server certificates for authentication.
+     */
+    MUTUAL(2),
+
+    /**
+     * Similar to the passthrough mode, except servers with this TLS mode
+     * do not require an associated VirtualService to map from the SNI
+     * value to service in the registry. The destination details such as
+     * the service/subset/port are encoded in the SNI value. The proxy
+     * will forward to the upstream (Envoy) cluster (a group of
+     * endpoints) specified by the SNI value. This server is typically
+     * used to provide connectivity between services in disparate L3
+     * networks that otherwise do not have direct connectivity between
+     * their respective endpoints. Use of this mode assumes that both the
+     * source and the destination are using Istio mTLS to secure traffic.
+     */
+    AUTO_PASSTHROUGH(3),
+
+    /**
+     * Secure connections from the downstream using mutual TLS by presenting
+     * server certificates for authentication.
+     * Compared to Mutual mode, this mode uses certificates, representing
+     * gateway workload identity, generated automatically by Istio for
+     * mTLS authentication. When this mode is used, all other fields in
+     * `TLSOptions` should be empty.
+     */
+    ISTIO_MUTUAL(4);
+
+    private final int intValue;
+
+    TLSOptionsMode(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int value() {
+        return intValue;
+    }
+}
