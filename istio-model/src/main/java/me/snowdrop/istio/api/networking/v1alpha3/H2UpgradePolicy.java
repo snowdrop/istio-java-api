c4324723d48201fbea221e34ddf4e53fe2d4a14e
Chris Laprun <metacosm@gmail.com>
2020-05-18 18:25:06 +0200

    feat: re-add generation for v1alpha3 networking entities

---
+++ istio-model/src/main/java/me/snowdrop/istio/api/networking/v1alpha3/H2UpgradePolicy.java
@@ -0,0 +1,31 @@
+package me.snowdrop.istio.api.networking.v1alpha3;
+
+public enum H2UpgradePolicy {
+
+    /**
+     * Use the global default.
+     */
+    DEFAULT(0),
+
+    /**
+     * Do not upgrade the connection to http2.
+     * This opt-out option overrides the default.
+     */
+    DO_NOT_UPGRADE(1),
+
+    /**
+     * Upgrade the connection to http2.
+     * This opt-in option overrides the default.
+     */
+    UPGRADE(2);
+
+    private final int intValue;
+
+    H2UpgradePolicy(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int value() {
+        return intValue;
+    }
+}
