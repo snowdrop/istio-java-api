6e52e40a3ed574be18e340f505596223a7ded999
Florian Wiesner <florian.wiesner@oracle.com>
2020-06-26 14:35:08 +0200

    Got a deserialization issue with ServerTLSSettingsProtocol

--- istio-model/src/main/java/me/snowdrop/istio/api/networking/v1beta1/ServerTLSSettingsProtocol.java
+++ istio-model/src/main/java/me/snowdrop/istio/api/networking/v1beta1/ServerTLSSettingsProtocol.java
@@ -18,6 +18,8 @@
  */
 package me.snowdrop.istio.api.networking.v1beta1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * TLS protocol versions.
  *
@@ -28,26 +30,31 @@ public enum ServerTLSSettingsProtocol {
     /**
      * Automatically choose the optimal TLS version.
      */
+    @JsonProperty("TLS_AUTO")
     Server_TLSOptions_TLS_AUTO(0),
 
     /**
      * TLS version 1.0
      */
+    @JsonProperty("TLSV1_0")
     Server_TLSOptions_TLSV1_0(1),
 
     /**
      * TLS version 1.1
      */
+    @JsonProperty("TLSV1_1")
     Server_TLSOptions_TLSV1_1(2),
 
     /**
      * TLS version 1.2
      */
+    @JsonProperty("TLSV1_2")
     Server_TLSOptions_TLSV1_2(3),
 
     /**
      * TLS version 1.3
      */
+    @JsonProperty("TLSV1_3")
     Server_TLSOptions_TLSV1_3(4);
 
     private final int intValue;
